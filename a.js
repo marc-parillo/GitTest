c51a7da402592a6d0c34c0cdca3d32e57a169cd5
| xargs echo <marc.parillo@ally.com>
2014-12-14 18:03:39 -0500

    Changed add function logic from && to ||

--- a.js
+++ a.js
@@ -3,7 +3,7 @@ var a = 1;
 var b = 2;
 
 function add(x,y) {
-  if (typeof x !== 'number' && typeof y !== 'number') {
+  if (typeof x !== 'number' || typeof y !== 'number') {
     return null;
   }
   return x+y;
